3f48a88d9857be1b526669e981c09b2c8c149ab7
vegetabledog <2813262902@qq.com>
2026-02-17 03:35:01 +0800

    LEETCODE EVERYDAY DONE!

---
+++ CS106B/TEXTBOOK/CHAPTER04/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+const double PI = 3.1415926;
+const double SPEED_OF_LIGHT = 2.9979E+8;
+const double FINE_STRUCTURE = 7.257355E-3;
+void printPrecisionTable();
+int main()
+{
+    return 0;
+}
